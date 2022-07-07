ea02a431413fc6eadcf00687d4c231309e6c60ec
Naz <bluspringindohq@gmail.com>
2022-07-07 14:36:00 +0800

    I've made many a terrible workaround in my time, but what the fuck

---
+++ src/main/java/xyz/bluspring/randomizedskyblock/StupidWorkaround.java
@@ -0,0 +1,91 @@
+package xyz.bluspring.randomizedskyblock;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.structure.StructureSet;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntryList;
+import net.minecraft.world.ChunkRegion;
+import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.Blender;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.VerticalBlockSample;
+import net.minecraft.world.gen.noise.NoiseConfig;
+import xyz.bluspring.randomizedskyblock.generator.VoidGenerator;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class StupidWorkaround extends ChunkGenerator {
+    public StupidWorkaround(Registry<StructureSet> structureSetRegistry, Optional<RegistryEntryList<StructureSet>> structureOverrides, BiomeSource biomeSource) {
+        super(structureSetRegistry, structureOverrides, biomeSource);
+    }
+
+    public static Codec<VoidGenerator> getVoidGeneratorCodec() {
+        return RecordCodecBuilder.create((instance) -> createStructureSetRegistryGetter(instance).apply(instance, instance.stable(VoidGenerator::new)));
+    }
+
+    @Override
+    protected Codec<? extends ChunkGenerator> getCodec() {
+        return null;
+    }
+
+    @Override
+    public void carve(ChunkRegion chunkRegion, long seed, NoiseConfig noiseConfig, BiomeAccess world, StructureAccessor structureAccessor, Chunk chunk, GenerationStep.Carver carverStep) {
+
+    }
+
+    @Override
+    public void buildSurface(ChunkRegion region, StructureAccessor structures, NoiseConfig noiseConfig, Chunk chunk) {
+
+    }
+
+    @Override
+    public void populateEntities(ChunkRegion region) {
+
+    }
+
+    @Override
+    public int getWorldHeight() {
+        return 0;
+    }
+
+    @Override
+    public CompletableFuture<Chunk> populateNoise(Executor executor, Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
+        return null;
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getMinimumY() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight(int x, int z, Heightmap.Type heightmap, HeightLimitView world, NoiseConfig noiseConfig) {
+        return 0;
+    }
+
+    @Override
+    public VerticalBlockSample getColumnSample(int x, int z, HeightLimitView world, NoiseConfig noiseConfig) {
+        return null;
+    }
+
+    @Override
+    public void getDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {
+
+    }
+}
---
+++ src/main/kotlin/xyz/bluspring/randomizedskyblock/generator/VoidGenerator.kt
@@ -0,0 +1,103 @@
+package xyz.bluspring.randomizedskyblock.generator
+
+import com.mojang.serialization.Codec
+import net.minecraft.block.Blocks
+import net.minecraft.structure.StructureSet
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.registry.BuiltinRegistries
+import net.minecraft.util.registry.Registry
+import net.minecraft.world.ChunkRegion
+import net.minecraft.world.HeightLimitView
+import net.minecraft.world.Heightmap
+import net.minecraft.world.biome.BiomeKeys
+import net.minecraft.world.biome.source.BiomeAccess
+import net.minecraft.world.biome.source.FixedBiomeSource
+import net.minecraft.world.chunk.Chunk
+import net.minecraft.world.gen.GenerationStep
+import net.minecraft.world.gen.StructureAccessor
+import net.minecraft.world.gen.chunk.Blender
+import net.minecraft.world.gen.chunk.ChunkGenerator
+import net.minecraft.world.gen.chunk.VerticalBlockSample
+import net.minecraft.world.gen.noise.NoiseConfig
+import xyz.bluspring.randomizedskyblock.StupidWorkaround
+import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
+
+class VoidGenerator(
+    structureSetRegistry: Registry<StructureSet>
+) : ChunkGenerator(structureSetRegistry, Optional.empty(), FixedBiomeSource(BuiltinRegistries.BIOME.getEntry(BiomeKeys.SNOWY_TAIGA).get())) {
+    override fun getCodec(): Codec<VoidGenerator> {
+        return CODEC
+    }
+
+    override fun carve(
+        chunkRegion: ChunkRegion,
+        seed: Long,
+        noiseConfig: NoiseConfig,
+        world: BiomeAccess,
+        structureAccessor: StructureAccessor,
+        chunk: Chunk,
+        carverStep: GenerationStep.Carver
+    ) {
+    }
+
+    override fun buildSurface(
+        region: ChunkRegion,
+        structures: StructureAccessor,
+        noiseConfig: NoiseConfig,
+        chunk: Chunk
+    ) {
+    }
+
+    override fun populateEntities(region: ChunkRegion) {
+    }
+
+    override fun getWorldHeight(): Int {
+        return 384
+    }
+
+    override fun populateNoise(
+        executor: Executor,
+        blender: Blender,
+        noiseConfig: NoiseConfig,
+        structureAccessor: StructureAccessor,
+        chunk: Chunk
+    ): CompletableFuture<Chunk> {
+        return CompletableFuture.completedFuture(chunk)
+    }
+
+    override fun getSeaLevel(): Int {
+        return -63
+    }
+
+    override fun getMinimumY(): Int {
+        return 0
+    }
+
+    override fun getHeight(
+        x: Int,
+        z: Int,
+        heightmap: Heightmap.Type,
+        world: HeightLimitView,
+        noiseConfig: NoiseConfig
+    ): Int {
+        return world.bottomY
+    }
+
+    override fun getColumnSample(
+        x: Int,
+        z: Int,
+        world: HeightLimitView,
+        noiseConfig: NoiseConfig
+    ): VerticalBlockSample {
+        return VerticalBlockSample(world.bottomY, arrayOf(Blocks.AIR.defaultState))
+    }
+
+    override fun getDebugHudText(text: MutableList<String>, noiseConfig: NoiseConfig, pos: BlockPos) {
+    }
+
+    companion object {
+        val CODEC: Codec<VoidGenerator> = StupidWorkaround.getVoidGeneratorCodec()
+    }
+}
